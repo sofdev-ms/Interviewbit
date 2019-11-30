93a03d7efb98458b15f9a1ad85af61b811574295
Mohit sharma <msharmajiit@gmail.com>
2019-11-30 19:53:11 +0530

    hotel bookings possible

---
+++ Array/hotel bookings possible.cpp
@@ -0,0 +1,29 @@
+//https://www.interviewbit.com/problems/hotel-bookings-possible/
+
+bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K) {
+    sort(arrive.begin() , arrive.end());
+    sort(depart.begin() , depart.end());
+    int n = arrive.size();
+    int present = 1;
+    int i = 1 ;
+    int j = 0;
+
+    while( i  < n  )
+    {
+        if(arrive[i] < depart[j])
+        {
+            i++;
+            present++;
+        }
+        else
+        {
+            j++;
+            present--;
+        }
+        if(present > K)
+        return 0;
+    }
+
+    return 1;
+}
+
